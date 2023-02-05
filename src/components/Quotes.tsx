5d30083917242be184a8d14e004641f1d5803c0d
Blake Eriks <blakeeriks.dev@gmail.com>
2023-02-04 22:43:49 -0500

    Rework quotes page to use book selector

--- src/components/Quotes.tsx
+++ src/components/Quotes.tsx
@@ -1,91 +1,77 @@
 
-import { Select, Table } from 'antd';
-import useQuotesApi from '../api/quote';
-import { Book } from '../types/types';
+import { BookOutlined } from '@ant-design/icons';
+import { Button, Menu, MenuProps } from 'antd';
+import Search from 'antd/es/input/Search';
+import { ColumnsType } from 'antd/es/table';
 import { useState } from 'react';
 import useBookApi from '../api/book';
-import useAuthorApi from '../api/author';
+import useQuotesApi from '../api/quote';
+import { Quote, Tag } from '../types/types';
+import Table from './Table';
 
 const Quotes = () => {
 
   const { allQuotes } = useQuotesApi()
   const { allBooks } = useBookApi()
-  const { allAuthors } = useAuthorApi()
-  const [bookFilter, setBookFilter] = useState<string>()
+  const [selected, setSelected] = useState('0')
 
-  const columns = [
-    {
-      title: 'Id',
-      dataIndex: 'id',
-      key: 'id',
-    },
-    {
-      title: 'Quotee',
-      dataIndex: 'quotee',
-      key: 'quotee',
-    },
-    {
-      title: 'Book',
-      dataIndex: 'source',
-      key: 'source',
-      render: (source: Book) => source ? source.title : '-'
-    },
+  const columns: ColumnsType<Quote> = [
     {
       title: 'Quote',
       dataIndex: 'content',
-      key: 'content',
+    },
+    {
+      title: 'Tags',
+      dataIndex: 'tags',
+      render: tags => 
+        <div className='flex flex-wrap'>
+          {tags.map((tag: Tag) => 
+            <span key={tag.id} className='rounded-md border border-gray-500 p-1 m-1'>
+              {tag.name}
+            </span>
+          )}
+        </div>,
+      width: 200
     },
   ]
 
-  // console.log(allQuotes.data)
-  const books = allBooks.data?.map(book => {
-    return {
-      value: book.title,
-      label: book.title
-    }
-  })
+  const items: MenuProps['items'] = allBooks.data?.map(
+    (book, index) => (
+      {
+        key: index,
+        label: <span>{book.title}</span>,
+      }
+    )
+  )
   
-  const authors = allAuthors.data?.map(author => {
-    return {
-      value: author.name,
-      label: author.name
-    }
-  })
-
+  const filteredQuotes = allQuotes.data?.map((quote, index) => ({...quote, key: index}))
+    .filter((quote) => quote.source?.id === allBooks.data?.at(Number(selected))?.id)
+    
   return (
-    <div className='flex flex-col'>
-      <div className='flex items-center p-2'>
-        <Select
-          showSearch
-          placeholder="Select a book"
-          optionFilterProp="children"
-          onChange={event => console.log(event)}
-          onSearch={bookFilter => setBookFilter(bookFilter)}
-          filterOption={(input, option) =>
-            (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
-          }
-          options={books}
-          className='w-64 mr-2'
-        />
-        <Select
-          showSearch
-          placeholder="Select an author"
-          optionFilterProp="children"
-          onChange={event => console.log(event)}
-          onSearch={bookFilter => setBookFilter(bookFilter)}
-          filterOption={(input, option) =>
-            (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
-          }
-          options={authors}
-          className='w-64'
+    <div className='flex h-full'>
+      <div className='h-full overflow-auto m-2'>
+        <div className='flex'>
+          <Button
+            icon={<BookOutlined />}
+          >
+            Book
+          </Button>
+          <Search className=''/>
+        </div>
+        <Menu
+          items={items}
+          className='overflow-auto'
+          onSelect={({ key }) => setSelected(key)}
+          selectedKeys={[selected]}
         />
       </div>
-      <Table
-        dataSource={allQuotes.data?.map((quote, index) => ({...quote, key: index}))}
-        columns={columns}
-        scroll={{ y: 600 }}
-        pagination={false}
-      />
+      <div className='flex-1'>
+        <Table
+          dataSource={filteredQuotes}
+          columns={columns}
+          pagination={false}
+        />
+      </div>
     </div>
   )
 }
