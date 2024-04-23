a2692a74ccb1e34c09386caffc316d3d5780b74d
Blake Eriks <blakeeriks.dev@gmail.com>
2024-04-23 14:04:03 -0400

    Support favorites

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,4 +1,6 @@
 import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useAtomValue } from 'jotai'
+import userAtom from 'state/user'
 
 export type User = {
   id: number
@@ -7,6 +9,38 @@ export type User = {
 
 const useUserApi = () => {
   const queryClient = useQueryClient()
+  const user = useAtomValue(userAtom)
+
+  const favorites = useQuery<number[]>({
+    queryKey: [user?.id, 'favorites'],
+    queryFn: () =>
+      fetch(`${process.env.REACT_APP_API_URL}/api/users/${user!.id}/favorites`).then(res =>
+        res.json()
+      ),
+    enabled: !!user,
+  })
+
+  const removeFavorite = async (quoteId: number) => {
+    const res = await fetch(
+      `${process.env.REACT_APP_API_URL}/api/users/${user!.id}/favorites/${quoteId}`,
+      {
+        method: 'DELETE',
+      }
+    )
+    queryClient.invalidateQueries([user?.id, 'favorites'])
+    return await res.json()
+  }
+
+  const addFavorite = async (quoteId: number) => {
+    const res = await fetch(
+      `${process.env.REACT_APP_API_URL}/api/users/${user!.id}/favorites/${quoteId}`,
+      {
+        method: 'POST',
+      }
+    )
+    queryClient.invalidateQueries([user?.id, 'favorites'])
+    return await res.json()
+  }
 
   return {
     allUsers: useQuery<User[]>(['users'], async () => {
@@ -24,6 +58,12 @@ const useUserApi = () => {
       queryClient.invalidateQueries(['users'])
       return await res.json()
     },
+
+    favorites: favorites.data,
+
+    removeFavorite,
+
+    addFavorite,
   }
 }
 
--- src/components/Quotes.tsx
+++ src/components/Quotes.tsx
@@ -1,10 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
 import { Menu, MenuProps } from 'antd'
 import useQuoteApi from 'api/quote'
+import useUserApi from 'api/user'
 import { useSetAtom } from 'jotai'
 import { cn } from 'lib/utils'
 import { find, first } from 'lodash'
-import { Copy, Edit, Edit2, Undo, X } from 'lucide-react'
+import { Copy, Edit, Edit2, Heart, Undo2, X } from 'lucide-react'
 import { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 import { useNavigate, useParams } from 'react-router-dom'
@@ -13,6 +14,7 @@ import useBookApi, { Book } from '../api/book'
 import { EditBookDialog } from './dialog/EditBook'
 import { EditQuoteDialog } from './dialog/EditQuote'
 import { Button } from './ui/button'
+import { Checkbox } from './ui/checkbox'
 
 const Quotes = () => {
   const { getBooks } = useBookApi()
@@ -21,9 +23,18 @@ const Quotes = () => {
   const setEditQuoteModalState = useSetAtom(modalStateAtom('editQuote'))
   const { save } = useQuoteApi()
   const [loading, setLoading] = useState({} as any)
-
+  const [hideDisabled, setHideDisabled] = useState(false)
+  const { favorites, addFavorite, removeFavorite } = useUserApi()
   const { bookId } = useParams()
   const navigate = useNavigate()
+
+  const toggleFavorite = (quoteId: number) => {
+    if (favorites?.includes(quoteId)) {
+      removeFavorite(quoteId)
+    } else {
+      addFavorite(quoteId)
+    }
+  }
 
   useEffect(() => {
     if (!bookId && books) {
@@ -42,9 +53,7 @@ const Quotes = () => {
     if (!quote) return
     setLoading({ ...loading, [quoteId]: true })
     save({ id: quoteId, deleted: !quote?.deleted }).then(() =>
-      setTimeout(() => {
-        setLoading({ ...loading, [quoteId]: false })
-      }, 500)
+      setLoading({ ...loading, [quoteId]: false })
     )
   }
 
@@ -78,63 +87,89 @@ const Quotes = () => {
         />
       </div>
       <div className='flex-1 overflow-auto'>
+        <Button
+          onClick={() => setHideDisabled(!hideDisabled)}
+          className='flex justify-end items-center p-2 px-4 space-x-2 border-b ml-auto'
+          variant='ghost'
+        >
+          <Checkbox checked={hideDisabled} />
+          <span>Hide Disabled</span>
+        </Button>
         <div className='w-full columns-3 overflow-y-auto'>
-          {activeBook?.quotes.map(({ content, id, quotee, deleted, createdAt }) => (
-            <div
-              key={id}
-              className={cn(
-                'flex p-6 pr-3 border-b border-r break-inside-avoid group',
-                loading[id] && 'opacity-50'
-              )}
-            >
-              <div className='my-auto flex-1'>
-                <div className='flex items-center mb-2' key={id}>
-                  <span
-                    className={cn('italic text-sm flex-1', deleted && 'line-through opacity-50')}
+          {activeBook?.quotes.map(({ content, id, quotee, deleted, createdAt }) =>
+            deleted && hideDisabled ? null : (
+              <div
+                key={id}
+                className={cn(
+                  'flex p-6 pr-3 border-b border-r break-inside-avoid group',
+                  loading[id] && 'opacity-50'
+                )}
+              >
+                <div className='my-auto flex-1'>
+                  <div className='flex items-center mb-2' key={id}>
+                    <span
+                      className={cn('italic text-sm flex-1', deleted && 'line-through opacity-50')}
+                    >
+                      "{content}"
+                    </span>
+                  </div>
+                  <div className='flex justify-between text-xs opacity-60'>
+                    <span>{new Date(createdAt).toLocaleDateString()}</span>
+                    {quotee && <div>{quotee}</div>}
+                  </div>
+                </div>
+                <div className='flex flex-col justify-center'>
+                  <Button
+                    variant='ghost'
+                    size='sm'
+                    disabled={loading[id]}
+                    onClick={() => toggleFavorite(id)}
+                    className='opacity-50 hover:opacity-80 transition-all scale-110'
                   >
-                    "{content}"
-                  </span>
-                </div>
-                <div className='flex justify-between text-xs opacity-60'>
-                  <span>{new Date(createdAt).toLocaleDateString()}</span>
-                  {quotee && <div>{quotee}</div>}
+                    <Heart
+                      className={cn('shrink-0')}
+                      size={12}
+                      fill={favorites?.includes(id) ? 'red' : 'none'}
+                    />
+                  </Button>
+                  <Button
+                    variant='ghost'
+                    size='sm'
+                    disabled={loading[id]}
+                    onClick={() => setEditQuoteModalState({ bookId: activeBook.id, quoteId: id })}
+                    className='opacity-50 hover:opacity-80 transition-all scale-110'
+                  >
+                    <Edit2 className='shrink-0' size={12} />
+                  </Button>
+                  <Button
+                    variant='ghost'
+                    size='sm'
+                    disabled={loading[id]}
+                    onClick={() => {
+                      navigator.clipboard.writeText(content)
+                      toast.success('Copied!')
+                    }}
+                    className='opacity-50 hover:opacity-80 transition-all scale-110'
+                  >
+                    <Copy className='shrink-0' size={12} />
+                  </Button>
+                  <Button
+                    variant='ghost'
+                    size='sm'
+                    disabled={loading[id]}
+                    onClick={() => toggleDisabled(id)}
+                    className='opacity-50 hover:opacity-80 transition-all scale-110'
+                  >
+                    {deleted ? (
+                      <Undo2 className='shrink-0' size={12} />
+                    ) : (
+                      <X className='shrink-0' size={12} />
+                    )}
+                  </Button>
                 </div>
               </div>
-              <div className='flex flex-col justify-center'>
-                <Button
-                  variant='ghost'
-                  size='sm'
-                  disabled={loading[id]}
-                  onClick={() => setEditQuoteModalState({ bookId: activeBook.id, quoteId: id })}
-                >
-                  <Edit2 className='opacity-0 group-hover:opacity-100 shrink-0' size={12} />
-                </Button>
-                <Button
-                  variant='ghost'
-                  size='sm'
-                  disabled={loading[id]}
-                  onClick={() => {
-                    navigator.clipboard.writeText(content)
-                    toast.success('Copied!')
-                  }}
-                >
-                  <Copy className='opacity-0 group-hover:opacity-100 shrink-0' size={12} />
-                </Button>
-                <Button
-                  variant='ghost'
-                  size='sm'
-                  disabled={loading[id]}
-                  onClick={() => toggleDisabled(id)}
-                >
-                  {deleted ? (
-                    <Undo className='opacity-0 group-hover:opacity-100 shrink-0' />
-                  ) : (
-                    <X className='opacity-0 group-hover:opacity-100 shrink-0' size={12} />
-                  )}
-                </Button>
-              </div>
-            </div>
-          ))}
+            )
+          )}
         </div>
       </div>
       <EditBookDialog />
